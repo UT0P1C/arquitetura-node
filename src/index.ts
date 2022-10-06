c8579a8fc87b68b06badd7d092d3ede1adffbae6
UT0P1C <ljonham@gmail.com>
2022-10-06 13:12:29 -0300

    estrutura básica

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Okay");
+import express, {Request, Response, NextFunction} from "express";
+
+const app = express();
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+	res.status(200).json({message: "okay"});
+})
+
+app.listen(3000, () => {
+	console.log("running application in port 3000");
+})
