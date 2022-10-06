2a7d34e940c814eec0dad2a1591682f1ca87bdf4
UT0P1C <ljonham@gmail.com>
2022-10-06 13:39:21 -0300

    primeira rota

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express, {Request, Response, NextFunction} from "express";
 
+import usersRoute from "./routes/usersRoute";
+
 const app = express();
+
+app.use(usersRoute);
 
 app.get("/status", (req: Request, res: Response, next: NextFunction) => {
 	res.status(200).json({message: "okay"});
